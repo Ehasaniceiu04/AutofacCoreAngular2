33e2886ab982f36d7b2ab5c2063838227d6d4def
Ehasanul Hoque <ehasan@outlook.com>
2016-11-26 21:40:37 +0600

    Autofac module commit

---
+++ src/Web.CoreApp/AutoFacModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Web.CoreApp.Model;
+using Web.CoreApp.Repository;
+
+namespace Web.CoreApp
+{
+    public class AutoFacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Employee>().As<IEmployee>();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.RegisterType<EngineerRepository>().As<IEngineerRepository>();
+            builder.RegisterType<ExperienceRepository>().As<IExperienceRepository>();
+            builder.RegisterType<EngineerContext>();
+        }
+    }
+}
