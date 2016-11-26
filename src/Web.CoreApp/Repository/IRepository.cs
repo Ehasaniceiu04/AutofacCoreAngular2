5761fb3fe826e3d18d6fa53adc89afb862657da7
Ehasanul Hoque <ehasan@outlook.com>
2016-11-26 20:29:46 +0600

    add base repository class

---
+++ src/Web.CoreApp/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.CoreApp.Repository
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity Add(TEntity entity);
+
+        bool Update(TEntity entity);
+
+        bool Delete(TEntity entity);
+
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+
+        ICollection<TEntity> GetAll();
+    }
+}
---
+++ src/Web.CoreApp/Repository/Repository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.CoreApp.Repository
+{
+    namespace DataAccess.Repository
+    {
+        using Microsoft.EntityFrameworkCore;
+        #region Namespaces
+
+        using System;
+        using System.Collections.Generic;
+        using System.Linq.Expressions;
+
+        #endregion Namespaces
+
+       
+        public class Repository<TEntity> : IRepository<TEntity>
+            where TEntity : class
+
+        {
+            protected readonly DbContext _context;
+            public Repository(DbContext context)
+            {
+                _context = context;
+            }
+
+            public TEntity Get(int id)
+            {
+                return _context.Set<TEntity>().Find(id);
+            }
+
+            public TEntity Get(Expression<Func<TEntity, bool>> filter)
+            {
+                return _context.Set<TEntity>().Where(filter).SingleOrDefault();
+            }
+
+            public IEnumerable<TEntity> GetAll()
+            {
+                return _context.Set<TEntity>().ToList();
+            }
+
+            public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+            {
+                return _context.Set<TEntity>().Where(predicate).ToList();
+            }
+
+            public void Add(TEntity entity)
+            {
+                _context.Set<TEntity>().Add(entity);
+            }
+
+            public void AddRange(IEnumerable<TEntity> entities)
+            {
+                _context.Set<TEntity>().AddRange(entities);
+            }
+
+            public void Delete(TEntity entity)
+            {
+                _context.Set<TEntity>().Remove(entity);
+            }
+
+            public void Update(TEntity entity)
+            {
+                _context.Set<TEntity>().Update(entity);
+            }
+
+            public void UpdateRange(IEnumerable<TEntity> entities)
+            {
+                _context.Set<TEntity>().UpdateRange(entities);
+            }
+
+            public void DeleteRange(IEnumerable<TEntity> entityCollection)
+            {
+                _context.Set<TEntity>().RemoveRange(entityCollection);
+            }
+        }
+    }
+}
