26a60a7cbd24b457af8f2da975ecf843ae9af871
Ehasanul Hoque <ehasan@outlook.com>
2016-12-01 23:38:44 +0600

    adding angular 2 configuration file

---
+++ src/CoreMVCAngular2App/Startup.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Web.CoreApp.Model;
+using Autofac;
+using Web.CoreApp;
+using Microsoft.EntityFrameworkCore;
+using Autofac.Extensions.DependencyInjection;
+using System;
+
+namespace CoreMVCApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
+            //Configuration = builder.Build();
+        }
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<EngineerContext>(options => options.UseSqlServer(@"server = localhost; database = IdentityServer4; trusted_connection = yes"));
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<AutoFacModule>();
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
+        }
+        public IConfigurationRoot Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/node_modules")
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
---
+++ src/CoreMVCAngular2App/UnitOfWork/EFUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.CoreApp.Model;
+
+namespace Web.CoreApp
+{
+    public class EFUnitOfWork : IEFUnitOfWork,IDisposable
+    {
+        private readonly EngineerContext _engineerContext;
+        private bool disposed = false;
+        public EFUnitOfWork(EngineerContext engineerContext)
+        {
+            _engineerContext = engineerContext;
+        }
+
+        void IEFUnitOfWork.Commit()
+        {
+            _engineerContext.SaveChanges();
+        }
+       
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _engineerContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
